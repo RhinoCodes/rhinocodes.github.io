77356e5e8d81a523ba1c8cac521b9ca680014a32
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2019-12-08 12:33:25 -0600

    upday

--- script.js
+++ script.js
@@ -20,11 +20,12 @@ function newLink(name, url){
       document.getElementById("last").innerHTML += "<form method='GET' action='"+url+"'>  <button>"+name+"</button>    </form>";
     }
 
+    function newBarLink(name, url){
+      document.getElementById("nav").innerHTML += "<li><a href='"+url+"'>"+name+"</a></li>";
+    }
   newLink("Docs", "https://www.docs.google.com/");
   newLink("Slides", "https://docs.google.com/presentation/u/0/?tgif=c");
   newLink("Forms", "https://docs.google.com/forms/u/0/?tgif=d");
   newLink("Drive", "https://www.drive.google.com");
   newLink("Classroom", "https://classroom.google.com/u/0/h");
   newLink("Hangouts", "https://hangouts.google.com/?safe=active");
-  br();
-  newLink("TrollBucks", "/trollbucks/")
